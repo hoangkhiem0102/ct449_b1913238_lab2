13d471a9c435d15a0d44e5745327e380675eb27e
unknown <https://github.com/TUNGB1913351/CT449_LAP2.git>
2022-09-30 09:23:35 +0700

    Cai dat cac handler truy xuat CSDL LAP 2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
+const ApiError = require("./app/api-error");
 const app = express();
 
 app.use(cors());
@@ -12,6 +13,19 @@ app.get("/", (req, res)=> {
 });
 
 const contactsRouter = require("./app/routes/contact.route");
+const { application } = require("express");
+
 app.use("/api/contacts", contactsRouter);
 
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) =>{
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    })
+}
+
+)
 module.exports = app;
